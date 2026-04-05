2f93d00ee3f0cfcfbf2d4de137a2881137d5caea
Nakamuro-unl <Nakamuro-unl@users.noreply.github.com>
2026-04-05 17:46:12 +0000

    deploy: auto build from f82ecfb11dbabb6d4fa71360bd9d45f77f9f58e8 Nakamuro-unl/GodotActionGame@f82ecfb11dbabb6d4fa71360bd9d45f77f9f58e8

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775410536|15834455';
+const CACHE_VERSION = '1775411170|3675988';
 /** @type {string} */
 const CACHE_PREFIX = 'Math Mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
